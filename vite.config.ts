189bf385492be49da65265ffb41e2bed8a34028e
vaibhav604 <vaibhavmane604@gmail.com>
2023-11-11 15:41:55 +0530

    adding dist

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/MoneyMarket/",
   // build: {
   //   assetsDir: "assets", // Change this to your preferred output directory for assets
   //   rollupOptions: {
